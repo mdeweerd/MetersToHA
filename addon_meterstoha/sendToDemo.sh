ef12fdf835cd923ace519f410c5eb3a6a76ab574
MDW <mdeweerd@users.noreply.github.com>
2023-11-04 00:04:48 +0100

    Upgrade base image to version 14.3.1

--- addon_meterstoha/sendToDemo.sh
+++ addon_meterstoha/sendToDemo.sh
@@ -3,3 +3,4 @@
 # "demo" HA server configured in .ssh/config as ha-demo.
 #scp -rp ./* ha-demo:/addons/haos-addon/
 scp -rp ./run.sh translations config.yaml ha-demo:/addons/haos-addon/
+#scp -rp Dockerfile ./run.sh translations config.yaml ha-demo:/addons/haos-addon/
