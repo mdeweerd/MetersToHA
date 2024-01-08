4eb9df9b0797d82b5dc77d9aa661e4005b41a17e
MDW <mdeweerd@users.noreply.github.com>
2024-01-09 00:54:32 +0100

    Increase run version to 17.0

--- addon_meterstoha/sendToDemo.sh
+++ addon_meterstoha/sendToDemo.sh
@@ -2,5 +2,6 @@
 # Private script to synchronize the development with
 # "demo" HA server configured in .ssh/config as ha-demo.
 #scp -rp ./* ha-demo:/addons/haos-addon/
-scp -rp ./run.sh translations config.yaml build.yaml ha-demo:/addons/haos-addon/
+scp -rp ./run.sh Dockerfile translations config.yaml build.yaml ha-demo:/addons/haos-addon/
+scp -rp ./rootfs/* ha-demo:/addons/haos-addon/rootfs/
 #scp -rp Dockerfile ./run.sh translations config.yaml ha-demo:/addons/haos-addon/
